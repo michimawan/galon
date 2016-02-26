0fec35d61e53ce44866092d40f358f178b2a9cf9
michimawan <blog.1393@gmail.com>
2016-02-26 09:07:09 +0700

    fix printdebt on customer

--- Controller/CustomersController.php
+++ Controller/CustomersController.php
@@ -1,8 +1,8 @@
 <?php
 class CustomersController extends AppController {
-	public $layout = 'layout';
+    public $layout = 'layout';
     public $uses = array('PairTeamCustomer', 'Customer');
-	public function index() {
+    public function index() {
         $this->set('title','Galon - Data Pelanggan');
         $this->paginate = array(
             'limit' => 20,
@@ -15,8 +15,8 @@ class CustomersController extends AppController {
 
     public function add(){
         $this->set('title','Galon - Tambah Data Pelanggan');
-    	if ($this->request->is('post')) {
-                 
+        if ($this->request->is('post')) {
+
             $this->Customer->create();
             $this->request->data['Customer']['kdpelanggan'] = $this->generate_kodepelanggan();
             if ($this->Customer->save($this->request->data)) {
@@ -24,23 +24,23 @@ class CustomersController extends AppController {
                 $this->redirect(array('action' => 'index'));
             } else {
                 $this->Session->setFlash('Pelanggan baru gagal dibuat, silahkan dicoba lagi', 'customflash', array('class' => 'warning'));
-            }   
+            }
         }
     }
 
     public function edit($id = null){
         $this->set('title','Galon - Ubah Data Pelanggan');
-    	if (!$id) {
+        if (!$id) {
             $this->Session->setFlash('Gagal memilih pelanggan yang akan diedit', 'customflash', array('class' => 'danger'));
             $this->redirect(array('action'=>'index'));
         }
- 
+
         $customer = $this->Customer->findById($id);
         if (!$customer) {
             $this->Session->setFlash('Data pelanggan yang diedit berbeda', 'customflash', array('class' => 'danger'));
             $this->redirect(array('action'=>'index'));
         }
- 
+
         if ($this->request->is('post') || $this->request->is('put')) {
             $this->Customer->id = $id;
             if ($this->Customer->save($this->request->data)) {
@@ -50,7 +50,7 @@ class CustomersController extends AppController {
                 $this->Session->setFlash('Gagal mengedit data pelanggan', 'customflash', array('class' => 'warning'));
             }
         }
- 
+
         if (!$this->request->data) {
             $this->request->data = $customer;
         }
@@ -61,7 +61,7 @@ class CustomersController extends AppController {
             if (!$id) {
                 $this->Session->setFlash('Tidak ada pelanggan yang dipilih', 'customflash', array('class' => 'warning'));
             }
-             
+
             $this->Customer->id = $id;
             if($this->Customer->exists()){
                 if ($this->Customer->saveField('status', 0)) {
@@ -69,7 +69,7 @@ class CustomersController extends AppController {
                     $this->redirect(array('action' => 'index'));
                 }
             }
-            
+
             $this->Session->setFlash('Data pelanggan gagal dihapus', 'customflash', array('class' => 'warning'));
         }
         $this->redirect(array('action' => 'index'));
@@ -79,24 +79,24 @@ class CustomersController extends AppController {
         $kdpelanggan = 'PG';
 
         $missing_code = str_pad($this->get_missing_number(), 4, '0', STR_PAD_LEFT);
-        
+
         return $kdpelanggan.$missing_code;
     }
 
     private function get_missing_number(){
         $datas = $this->Customer->find('all', array('fields' => 'SUBSTRING(kdpelanggan, 3) AS kdpelanggan', 'order' => 'kdpelanggan'));
-        
+
         $missing_code = 1;
         if(count($datas) == 0)
             return $missing_code;
-        
+
         for ($i = 0; $i < number_format($datas[count($datas)-1][0]['kdpelanggan']); $i++ ) {
             if(number_format($datas[$i][0]['kdpelanggan']) != $missing_code)
                 return $missing_code;
 
             $missing_code++;
         }
-        
+
         return $missing_code;
     }
 
@@ -115,37 +115,71 @@ class CustomersController extends AppController {
         } else {
             $this->redirect(array('action' => 'index'));
         }
-        
+
         //$this->redirect(array('action' => 'autocompletes'));
     }
 
+    public function printdebt($idtim = null){
+        $this->set('title', 'Galon - Daftar Piutang Pelanggan');
+
+        $tim['Customer.status'] = 1;
+        if($idtim)
+            $tim['PairTeamCustomer.idtim'] = $idtim;
+
+        $customers = $this->PairTeamCustomer->find('all', array(
+            'limit' => 20,
+            'conditions' => array(
+                'OR' => array(
+                    'Customer.hutang > ' => 0,
+                    'Customer.galonterpinjam > ' => 0),
+                'AND' => $tim
+            ),
+            'fields' => array('PairTeamCustomer.idtim', 'PairTeamCustomer.idcustomer', 'Customer.harikunjungan',
+            'Customer.id', 'Customer.kdpelanggan', 'Customer.namapelanggan', 'Customer.alamat', 'Customer.hutang', 'Customer.galonterpinjam', 'Customer.transaksiterakhir'),
+            'recursive' => 0
+        ));
+        if($idtim) {
+            $team = $this->PairTeamCustomer->Team->find('all', array(
+                'conditions' => array('Team.idtim' => $idtim)
+            ));
+            $team = "Daftar Piutang Sales " . $team[0]['User']['firstname'] .
+                " dan " . $team[1]['User']['firstname'];
+        }
+        else
+            $team = "Daftar Semua Piutang Pelanggan";
+
+        $this->set(compact('customers'));
+        $this->set(compact('team'));
+
+        $this->layout = 'print';
+    }
 
     // admin only
     public function debt($idtim = null){
         $this->set('title', 'Galon - Daftar Piutang Pelanggan');
         $teams = array();
-        $user = $this->Auth->User(); 
+        $user = $this->Auth->User();
         if($user['role'] == 'pegawai')
             $idtim = $user['Team']['idtim'];
-        
+
         if($idtim){
             $this->paginate = array(
-                    'limit' => 20,
-                    // 'order' => array('Customer.hutang' => 'DESC' ),
-                    'conditions' => array('OR' => array('Customer.hutang > ' => 0, 'Customer.galonterpinjam > ' => 0), 'AND' => array('Customer.status' => 1, 'PairTeamCustomer.idtim' => $idtim)),
-                    'fields' => array('PairTeamCustomer.idtim', 'PairTeamCustomer.idcustomer', 'Customer.harikunjungan',
-                                'Customer.id', 'Customer.kdpelanggan', 'Customer.namapelanggan', 'Customer.alamat', 'Customer.hutang', 'Customer.galonterpinjam', 'Customer.transaksiterakhir'),
-                    'recursive' => 0
-                );
-                $customers = $this->paginate('PairTeamCustomer');
-                $teams = $this->Customer->PairTeamCustomer->Team->find('all', array('order' => 'idtim','conditions' => array('Team.status' => 1, 'Team.idtim' => $idtim), 'recursive' => 0));
+                'limit' => 20,
+                // 'order' => array('Customer.hutang' => 'DESC' ),
+                'conditions' => array('OR' => array('Customer.hutang > ' => 0, 'Customer.galonterpinjam > ' => 0), 'AND' => array('Customer.status' => 1, 'PairTeamCustomer.idtim' => $idtim)),
+                'fields' => array('PairTeamCustomer.idtim', 'PairTeamCustomer.idcustomer', 'Customer.harikunjungan',
+                'Customer.id', 'Customer.kdpelanggan', 'Customer.namapelanggan', 'Customer.alamat', 'Customer.hutang', 'Customer.galonterpinjam', 'Customer.transaksiterakhir'),
+                'recursive' => 0
+            );
+            $customers = $this->paginate('PairTeamCustomer');
+            $teams = $this->Customer->PairTeamCustomer->Team->find('all', array('order' => 'idtim','conditions' => array('Team.status' => 1, 'Team.idtim' => $idtim), 'recursive' => 0));
         } else {
             $this->paginate = array(
                 'limit' => 20,
                 // 'order' => array('Customer.hutang' => 'DESC' ),
                 'conditions' => array('OR' => array('Customer.hutang > ' => 0, 'Customer.galonterpinjam > ' => 0), 'AND' => array('Customer.status' => 1)),
                 'fields' => array('PairTeamCustomer.idtim', 'PairTeamCustomer.idcustomer', 'Customer.harikunjungan',
-                            'Customer.id', 'Customer.kdpelanggan', 'Customer.namapelanggan', 'Customer.alamat', 'Customer.hutang', 'Customer.galonterpinjam', 'Customer.transaksiterakhir'),
+                'Customer.id', 'Customer.kdpelanggan', 'Customer.namapelanggan', 'Customer.alamat', 'Customer.hutang', 'Customer.galonterpinjam', 'Customer.transaksiterakhir'),
                 'recursive' => 0
             );
             $customers = $this->paginate('PairTeamCustomer');
@@ -167,7 +201,7 @@ class CustomersController extends AppController {
             if(!isset($list_team[$team['Team']['idtim']])){
                 $list_team[$team['Team']['idtim']] = $team['User']['firstname']." ";
             }
-            else 
+            else
                 $list_team[$team['Team']['idtim']] .= $team['User']['firstname'];
         }
         return $list_team;
---
+++ View/Customers/printdebt.ctp
@@ -0,0 +1,55 @@
+<!-- app/View/Customers/debt.ctp -->
+
+
+<div class="row">
+
+    <div class="col-xs-12 col-md-10">
+    <h1><?php echo $team ?></h1>
+
+    <div class='table-responsive'>
+    <table class='table table-condensed table-hover table-stripped'>
+        <thead>
+            <tr>
+                <th>No.</th>
+                <th>Kode Pelanggan</th>
+                <th>Nama Pelanggan</th>
+                <th>Alamat</th>
+                <th>Hutang</th>
+                <th>Pinjam Galon</th>
+                <th>Hari Kunjungan</th>
+                <th>Transaksi Terakhir</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            if(!$customers){
+            ?>
+            <tr>
+                <td colspan=4>Belum ada pelanggan yang memiliki piutang</td>
+            </tr>
+            <?php
+            }
+            else {
+            ?>
+            <?php $count=0; ?>
+            <?php foreach($customers as $customer):
+                $count ++;
+            ?>
+            <tr>
+                <td><?php echo $count; ?></td>
+                <td><?php echo $customer['Customer']['kdpelanggan'];?></td>
+                <td><?php echo $customer['Customer']['namapelanggan'];?></td>
+                <td><?php echo $customer['Customer']['alamat']?></td>
+                <td><?php echo $customer['Customer']['hutang']; ?></td>
+                <td><?php echo $customer['Customer']['galonterpinjam'];?></td>
+                <td><?php echo $customer['Customer']['harikunjungan'];?></td>
+                <td><?php echo $customer['Customer']['transaksiterakhir'];?></td>
+            </tr>
+            <?php endforeach; ?>
+            <?php unset($customer); } ?>
+        </tbody>
+    </table>
+    </div>
+
+    </div>
+</div>
